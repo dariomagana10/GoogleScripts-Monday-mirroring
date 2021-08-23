b844a01d6f3ea45a18c715d5a051bba43dc6b2b7
Miguel Darío Magaña Heredia <mar_10dario@hotmail.com>
2021-08-23 16:12:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -23,32 +23,49 @@ function makeAPICall(key, query, variables) {
   return response;
 }
 
-function getboards() {
-  var query = "query{boards(limit:1000,state:active){id,name}}";
-  var variables = {
-      "limit" : 1000,
-      "state" : "active",
-    };
+//Function to delete items that are not on the desired state
+function DeleteFunction() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var s = ss.getSheetByName('mapeo');
+  var r = s.getRange('C:C');
+  var v = r.getValues();
+  for(var i=v.length-1;i>=0;i--)
+    if(v[0,i]=='Pendiente'||v[0,i]=='Enviar correo')
+      s.deleteRow(i+1);
+};
 
-  var ss = SpreadsheetApp.getActiveSpreadsheet(); 
-  var sheet = ss.getSheetByName("mapeo"); 
- 
+
+//Removing duplicates
+function RemoveDuplicates() {
+  var sheet=SpreadsheetApp.getActiveSheet();
+  var rows=sheet.getLastRow();
+  var firstColumn=sheet.getRange(1, 2, rows, 1).getValues();
+  firstColumn = firstColumn.map(function(e){return e[0]})
+  var uA=[];
+  for (var i=rows;i>0;i--) {
+    if (uA.indexOf(firstColumn[i-1])!=-1) {
+      sheet.deleteRow(i);
+    }else{
+      uA.push(firstColumn[i-1]);
+    }
+  }
+}
+
+function callAPI(){
   
-  var data = JSON.parse(makeAPICall(mondayAPIkey,query,variables));
-  var boards = data.data.boards;
-  var x = 2; //The row I want to start with (I have a header in row 1).
-  boards.forEach(function(elem){
-    sheet.getRange(x, 1).setValue(elem['name']);
-    sheet.getRange(x, 2).setValue(elem['id']);
-    x++; //this allows it to go to the next line before starting the loop over for the next item.
-  });
- 
-} 
+}
+
+function setFile(){
+
+}
+
+function updateState() {
+
+}
 
 function getitems() {
-  //var query = "query{items(limit:10){id,name,board{id,name}}}"; 
   var query = "query{boards(ids:1601895903, limit:200){id, name, items(limit:200){id,name, column_values{id,text}}}}"; 
-  var query = "query { items_by_column_values (board_id: 1601895903, column_id: \"status\", column_value: \"Generar archivo\") {id, name} }";
+  var query = "query { items_by_column_values (board_id: 1601895903, column_id: \"status\", column_value: \"Generar archivo\") {id, name, updated_at, column_values{id,text}} }";
 
   var variables = {
       "ids" :1601895903,
@@ -64,27 +81,42 @@ function getitems() {
   Logger.log(data) 
 
   var boards = data.data.boards;
+  var items_by_column_values = data.data.items_by_column_values;
  
   var x = 2; //Because of the headers
-
-      var items = boards['items']
+  //var items = boards['items']
       
-        for (var item in items){
-          var column_values = items[item]['column_values']
-          y = 3; 
+  for (var item in items_by_column_values){
+      var column_values = items_by_column_values[item]['column_values']
+      y = 4; 
 
-          for( var column_value in column_values){  
-            sheet.getRange(1, y).setValue(column_values[column_value]['id']);
-            sheet.getRange(x, 2).setValue(items[item]['name']);
-            sheet.getRange(x, 1).setValue(items[item]['id']);
-            sheet.getRange(1, 2).setValue('name');
-            sheet.getRange(1, 1).setValue('id');
+        for( var column_value in column_values){  
+          sheet.getRange(1, y).setValue(column_values[column_value]['id']);
+          sheet.getRange(x, 3).setValue(items_by_column_values[item]['updated_at']);
+          sheet.getRange(x, 2).setValue(items_by_column_values[item]['name']);
+          sheet.getRange(x, 1).setValue(items_by_column_values[item]['id']);
+          sheet.getRange(1, 3).setValue('updated_at');
+          sheet.getRange(1, 2).setValue('name');
+          sheet.getRange(1, 1).setValue('id');
 
-            sheet.getRange(x, y).setValue(column_values[column_value]['text']);
-            y++;
+          sheet.getRange(x, y).setValue(column_values[column_value]['text']);
+          y++;
           }
-    x++;
+      var file = DriveApp.getFilesByName(items_by_column_values[item]['id'])
+      var checkExistence = file.hasNext()
+      if (checkExistence === true){
+        Logger.log('it exist') 
+      }
+      else{
+        var createDocument = createDocFromForm(); 
+        Logger.log('does not exist') 
+      }     
+        x++;
     }
+
+var deleteFunction = DeleteFunction();
+var removeDuplicates =RemoveDuplicates();
+
 
 }
 
