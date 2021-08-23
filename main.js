b98a7f82d534afe3dee08f9089f9516d90208dcb
Miguel Darío Magaña Heredia <mar_10dario@hotmail.com>
2021-08-23 14:24:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // API key (for illustration only, you should store production key in an environment variable)
-  var mondayAPIkey  = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjExNTcxODY0MywidWlkIjoyMTUxMzI1NCwiaWFkIjoiMjAyMS0wNy0wMVQxODo1MTozNS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6NTkwMDQxLCJyZ24iOiJ1c2UxIn0.5ZEZQ6dILI-vwUfPPle-hUIbbP7l2s9g9QFcdwxNhAA";
-  var url = "https://api.monday.com/v2";
+var mondayAPIkey  = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjExNTcxODY0MywidWlkIjoyMTUxMzI1NCwiaWFkIjoiMjAyMS0wNy0wMVQxODo1MTozNS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6NTkwMDQxLCJyZ24iOiJ1c2UxIn0.5ZEZQ6dILI-vwUfPPle-hUIbbP7l2s9g9QFcdwxNhAA";
+var url = "https://api.monday.com/v2";
 
+
+//Main API call
 function makeAPICall(key, query, variables) {
   var url = "https://api.monday.com/v2";
   var options = {
@@ -30,6 +32,7 @@ function getboards() {
 
   var ss = SpreadsheetApp.getActiveSpreadsheet(); 
   var sheet = ss.getSheetByName("mapeo"); 
+ 
   
   var data = JSON.parse(makeAPICall(mondayAPIkey,query,variables));
   var boards = data.data.boards;
@@ -44,14 +47,17 @@ function getboards() {
 
 function getitems() {
   //var query = "query{items(limit:10){id,name,board{id,name}}}"; 
-  var query = "query{boards(ids:904548154, limit:10){id, name, items(limit:10){id,name}}}"; 
+  var query = "query{boards(ids:1601895903, limit:200){id, name, items(limit:200){id,name, column_values{id,text}}}}"; 
+  var query = "query { items_by_column_values (board_id: 1601895903, column_id: \"status\", column_value: \"Generar archivo\") {id, name} }";
+
   var variables = {
-      "ids" :904548154,
-      "limit" : 10,
+      "ids" :1601895903,
+      "limit" : 200,
       };
 
   var ss = SpreadsheetApp.getActiveSpreadsheet(); //I put my code in the Google Scripts editor on the Spreadsheet I wanted to use. 
   var sheet = ss.getSheetByName("mapeo"); //The sheet I am putting my Boards on.
+  
   
   var data = JSON.parse(makeAPICall(mondayAPIkey,query,variables)); 
 
@@ -59,20 +65,26 @@ function getitems() {
 
   var boards = data.data.boards;
  
-  var x = 2; //Because of headers
-  
+  var x = 2; //Because of the headers
 
-    for (var board in boards){
-    var items = boards[board]['items']
-    for (var item in items){
-    sheet.getRange(x, 1).setValue(boards[board]['name']);
-    sheet.getRange(x, 2).setValue(boards[board]['id']);
-    sheet.getRange(x, 3).setValue(items[item]['name'])
-    sheet.getRange(x, 4).setValue(items[item]['id']);
-    sheet.getRange(x, 5).setValue(item);
+      var items = boards['items']
+      
+        for (var item in items){
+          var column_values = items[item]['column_values']
+          y = 3; 
+
+          for( var column_value in column_values){  
+            sheet.getRange(1, y).setValue(column_values[column_value]['id']);
+            sheet.getRange(x, 2).setValue(items[item]['name']);
+            sheet.getRange(x, 1).setValue(items[item]['id']);
+            sheet.getRange(1, 2).setValue('name');
+            sheet.getRange(1, 1).setValue('id');
+
+            sheet.getRange(x, y).setValue(column_values[column_value]['text']);
+            y++;
+          }
     x++;
     }
-  }
 
 }
 
